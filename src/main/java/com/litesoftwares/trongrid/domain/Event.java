aee92db47e69ae9fead6186b72b050b94e977ad9
Philipinho <16838612+Philipinho@users.noreply.github.com>
2019-06-18 21:49:17 +0100

    Fix Event POJO

--- src/main/java/com/litesoftwares/trongrid/domain/Event.java
+++ src/main/java/com/litesoftwares/trongrid/domain/Event.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @lombok.Data
@@ -24,9 +25,9 @@ public class Event {
         @JsonProperty("transaction_id")
         private String transactionId;
         @JsonProperty("result")
-        private Result result;
+        private Map<String, Object> result;
         @JsonProperty("result_type")
-        private ResultType resultType;
+        private Map<String, Object> resultType;
         @JsonProperty("block_timestamp")
         private long blockTimestamp;
         @JsonProperty("block_number")
@@ -48,32 +49,6 @@ public class Event {
         private long at;
         @JsonProperty("page_size")
         private long pageSize;
-
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @lombok.Data
-    public static class Result {
-
-        @JsonProperty("from")
-        private String from;
-        @JsonProperty("to")
-        private String to;
-        @JsonProperty("value")
-        private String value;
-
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @lombok.Data
-    public static class ResultType {
-
-        @JsonProperty("from")
-        private String from;
-        @JsonProperty("to")
-        private String to;
-        @JsonProperty("value")
-        private String value;
 
     }
 
